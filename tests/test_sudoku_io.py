9b988f619639ad1047b3b5dd3897fc5bc3ed7837
Eric Lopez <pikurrot@gmail.com>
2023-10-04 10:27:55 +0200

    Add write_sudoku() and its unit test
    Also change some variable names

--- tests/test_sudoku_io.py
+++ tests/test_sudoku_io.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import os
-from utils.sudoku_io import read_sudoku
+from utils.sudoku_io import read_sudoku, write_sudoku
 
 class TestSudokuIO(unittest.TestCase):
 	
@@ -17,11 +17,18 @@ class TestSudokuIO(unittest.TestCase):
 			[0, 0, 0, 4, 1, 9, 0, 0, 5],
 			[0, 0, 0, 0, 8, 0, 0, 7, 9]
 		], dtype=np.int16)
-		self.file_path = os.path.join('tests', 'example.suk')
+		self.input_path = os.path.join('tests', 'test_data', 'example.suk')
+		self.output_path = os.path.join('tests', 'test_data', 'example_out.suk')
 
 	def tearDown(self):
-		pass
+		os.remove(self.output_path)
 
 	def test_read_sudoku(self):
-		read_array = read_sudoku(self.file_path)
+		read_array = read_sudoku(self.input_path)
 		np.testing.assert_array_equal(read_array, self.sudoku_array)
+
+	def test_write_sudoku(self):
+		write_sudoku(self.output_path, self.sudoku_array)
+		read_array = read_sudoku(self.output_path)
+		np.testing.assert_array_equal(read_array, self.sudoku_array)
+		
--- utils/sudoku_io.py
+++ utils/sudoku_io.py
@@ -1,16 +1,27 @@
 import numpy as np
 import os
 
-def read_sudoku(filename: str) -> np.ndarray:
+def read_sudoku(file_path: str) -> np.ndarray:
 	"""
-	Parses a sudoku puzzle from a file into a NumPy array.
+	Parses a sudoku from a file into a NumPy array.
 	"""
-	with open(filename, 'r') as f:
+	with open(file_path, 'r') as f:
 		lines = f.readlines()
 	lines = [l.strip() for l in lines]
-	puzzle = np.zeros((9, 9), dtype=np.int16)
+	board = np.zeros((9, 9), dtype=np.int16)
 	for i, line in enumerate(lines):
 		for j, char in enumerate(line):
 			if char != '.':
-				puzzle[i, j] = int(char)
-	return puzzle
+				board[i, j] = int(char)
+	return board
+
+def write_sudoku(file_path: str, board: np.ndarray) -> None:
+	"""
+	Writes a sudoku from a NumPy array into a file.
+	"""
+	with open(file_path, 'w') as f:
+		for row in board:
+			for num in row:
+				if num == 0: f.write('.')
+				else: f.write(str(num))
+			f.write('\n')
