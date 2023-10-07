10894b387c3f2203ab79579b82d44a4d94a74ab7
Eric Lopez <pikurrot@gmail.com>
2023-10-07 14:47:03 +0200

    Update run_solver.bat

--- run_solver.bat
+++ run_solver.bat
@@ -4,6 +4,14 @@
 set /p FileName="Enter the name of the .suk file (without extension): "
 
 :: Run the solver
-python sudoku1.py < input\%FileName%.suk > output\%FileName%.cnf
+python sudoku1.py < input\%FileName%.suk > dimacs\%FileName%.cnf
+
+:: Check for errors
+if %ERRORLEVEL% neq 0 (
+    echo An error occurred while running sudoku1.py
+    exit /b %ERRORLEVEL%
+) else (
+	echo CNF built successfully and saved in dimacs\%FileName%.cnf
+)
 
 pause
