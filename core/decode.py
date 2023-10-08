e9a697f23d7eb00a0a65bf0d144ea40e8f2f0f9f
Eric Lopez <pikurrot@gmail.com>
2023-10-08 13:06:32 +0200

    Add var2board() and its unit test

--- core/decode.py
+++ core/decode.py
@@ -14,3 +14,11 @@ def dimacs2var(dimacs: str) -> np.ndarray:
 				break
 			vars = np.append(vars, int(literal))
 	return vars
+
+def var2board(vars: np.ndarray) -> np.ndarray:
+	"""
+	Converts an array of variables to a sudoku board.
+	"""
+	var_tensor = vars.reshape((9, 9, 9)) # put variables back to a tensor
+	board = np.argmax(var_tensor, axis=2) + 1 # get the index of the only positive value in each cell
+	return board
