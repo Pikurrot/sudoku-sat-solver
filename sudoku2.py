1cac55a452a5fee9aac459ac2b07a29a9af6be5e
Eric Lopez <pikurrot@gmail.com>
2023-10-08 13:49:45 +0200

    Define main() in sudoku2.py

---
+++ sudoku2.py
@@ -0,0 +1,16 @@
+import sys
+from core.decode import dimacs2var, var2board
+from utils.sudoku_io import write_sudoku
+
+def main():
+	# read DIMACS
+	dimacs = sys.stdin.read()
+	# convert to variables
+	vars = dimacs2var(dimacs)
+	# convert to board
+	board = var2board(vars)
+	# write to file
+	write_sudoku(sys.stdout, board)
+
+if __name__ == '__main__':
+	main()
